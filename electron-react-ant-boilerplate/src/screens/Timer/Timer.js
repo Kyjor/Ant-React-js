734f911438f95c3e76be7f2ec7e5cfc708ee6bfe
Kyle <kyleconel@gmail.com>
2021-02-22 22:30:05 -0500

    timer page has a second counter

--- electron-react-ant-boilerplate/src/screens/Timer/Timer.js
+++ electron-react-ant-boilerplate/src/screens/Timer/Timer.js
@@ -1,30 +1,45 @@
-// Libs
-import React, { Component } from "react";
-// Styles
-import styles from "./Timer.scss";
-// Layouts
-import Layout from "@/layouts/App";
-// Components
-//import Path from "@/components/Timer/Path";
+import React, { useState, useEffect } from 'react';
+import s from './Timer.scss';
+const Timer = () => {
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
 
-/**
- * Home
- *
- * @class Timer
- * @extends {Component}
- */
-class Timer extends Component {
-  constructor(props) {
-    super(props);
+  function toggle() {
+    setIsActive(!isActive);
   }
 
-  render() {
-    return (
-        <div className={styles.timer}>
-        Hello
-        </div>
-    );
+  function reset() {
+    setSeconds(0);
+    setIsActive(false);
   }
-}
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds(seconds => seconds + 1);
+      }, 1000);
+    } else if (!isActive && seconds !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, seconds]);
+
+  return (
+    <div className="app">
+      <div className="time">
+        {seconds}s
+      </div>
+      <div className="row">
+        <button className={`button button-primary button-primary-${isActive ? 'active' : 'inactive'}`} onClick={toggle}>
+          {isActive ? 'Pause' : 'Start'}
+        </button>
+        <button className="button" onClick={reset}>
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Timer;
